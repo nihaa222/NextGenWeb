a1df8343fefe9d8e004dcc651267e56996969f0c
Niharika Deb <127780989+nihaa222@users.noreply.github.com>
2024-05-02 21:27:50 +0530

    last changes

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -6,7 +6,7 @@ import { Button } from "@mui/material";
 
 const Reviews = ({ id }) => {
   return (
-    <section id={id} className="">
+    <section id={id} className=" w-full">
       <div className="mt-[100px]">
         <div className="flex mb-5 items-center justify-center gap-2">
           <img className="h-4 w-4" src="none.png" alt="none" />
@@ -20,112 +20,114 @@ const Reviews = ({ id }) => {
         </p>
         <Acco />
       </div>
-      <div className="max-w-[1500px] text-white md:px-20 py-20 sm:mx-auto mx-[10px] md:mx-[30px] px-10 bg-[#150808d6] rounded-3xl">
-        {/* do here */}
-        <div className="  grid md:grid-cols-gridfot1">
-          <div className="cols-span-3 md:col-span-0">
-            <div className="flex items-center gap-3 ">
-              <div className="w-10 h-10 bg-orange-500 mb-8 rounded-full"></div>
-              <p className="font-bold text-[32px] mb-7">Twist</p>
-            </div>
-            <p className="text-white text-start font-bold text-[16px]">
-              Discover our seamless, powerful tools for effortless job searching
-              and hiring success"
-            </p>
-            <p className="text-start">
-              Book a free consultation call today to learn more about our
-              services
-            </p>
-            <div></div>
-
-            <form className="mt-8 ">
-              <div>
-                <label>
-                  NAME
-                  <input
-                    className="mb-4 ml-2 rounded-xl px-3 py-2"
-                    placeholder="name"
-                  />
-                </label>
+      <div className="w-full flex justify-center">
+        <div className="max-w-[1300px] text-white md:px-20 py-20 mx-auto mx-[10px] md:mx-[30px] px-10 bg-[#150808d6] rounded-3xl">
+          {/* do here */}
+          <div className="  grid md:grid-cols-gridfot1 gap-5">
+            <div className="cols-span-3 md:col-span-0">
+              <div className="flex items-center gap-3 ">
+                <div className="w-10 h-10 bg-orange-500 mb-8 rounded-full"></div>
+                <p className="font-bold text-[32px] mb-7">Twist</p>
               </div>
-              <div>
-                <label>
-                  EMAIL
-                  <input
-                    className="ml-2 rounded-xl px-3 py-2"
-                    placeholder="email"
-                  />
-                </label>
-                <Button
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "orange",
-                    borderRadius: "12px",
-                    marginTop: "20px",
-                  }}
-                  variant="contained"
-                  type="submit"
-                >
-                  Submit
-                </Button>
-              </div>
+              <p className="text-white text-start font-bold text-[16px]">
+                Discover our seamless, powerful tools for effortless job
+                searching and hiring success"
+              </p>
+              <p className="text-start">
+                Book a free consultation call today to learn more about our
+                services
+              </p>
+              <div></div>
 
-              {/* Any other form fields you have */}
-            </form>
-          </div>
-          <div className="grid grid-cols-3 mt-[50px]">
-            <div className="flex flex-col">
-              <a className="font-bold" href="#">
-                Template
-              </a>
-              <a className="font-semibold" href="#">
-                Features
-              </a>
-              <a className="font-semibold" href="#">
-                Tutorials
-              </a>
-              <a className="font-semibold" href="#">
-                Purchase
-              </a>
-              <a className="font-semibold" href="#">
-                Showcase
-              </a>
+              <form className="mt-8 ">
+                <div>
+                  <label>
+                    NAME
+                    <input
+                      className="mb-4 ml-2 rounded-xl px-3 py-2"
+                      placeholder="name"
+                    />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    EMAIL
+                    <input
+                      className="ml-2 rounded-xl px-3 py-2"
+                      placeholder="email"
+                    />
+                  </label>
+                  <Button
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: "orange",
+                      borderRadius: "12px",
+                      marginTop: "20px",
+                    }}
+                    variant="contained"
+                    type="submit"
+                  >
+                    Submit
+                  </Button>
+                </div>
+
+                {/* Any other form fields you have */}
+              </form>
             </div>
-            <div className="flex flex-col">
-              <a className="font-bold" href="#">
-                Connect
-              </a>
-              <a className="font-semibold" href="#">
-                Instagram
-              </a>
-              <a className="font-semibold" href="#">
-                Youtube
-              </a>
-              <a className="font-semibold" href="#">
-                Facebook
-              </a>
-              <a className="font-semibold" href="#">
-                Linkedin
-              </a>
-            </div>
-            <div className="flex flex-col">
-              <a className="font-bold" href="#">
-                Links
-              </a>
-              <a className="font-semibold" href="#">
-                Home
-              </a>
-              <a className="font-semibold" href="#">
-                Privacy
-              </a>
-              <a className="font-semibold" href="#">
-                Terms
-              </a>
-              <a className="font-semibold" href="#">
-                404
-              </a>
+            <div className="grid grid-cols-3 mt-[50px]">
+              <div className="flex flex-col">
+                <a className="font-bold" href="#">
+                  Template
+                </a>
+                <a className="font-semibold" href="#">
+                  Features
+                </a>
+                <a className="font-semibold" href="#">
+                  Tutorials
+                </a>
+                <a className="font-semibold" href="#">
+                  Purchase
+                </a>
+                <a className="font-semibold" href="#">
+                  Showcase
+                </a>
+              </div>
+              <div className="flex flex-col">
+                <a className="font-bold" href="#">
+                  Connect
+                </a>
+                <a className="font-semibold" href="#">
+                  Instagram
+                </a>
+                <a className="font-semibold" href="#">
+                  Youtube
+                </a>
+                <a className="font-semibold" href="#">
+                  Facebook
+                </a>
+                <a className="font-semibold" href="#">
+                  Linkedin
+                </a>
+              </div>
+              <div className="flex flex-col">
+                <a className="font-bold" href="#">
+                  Links
+                </a>
+                <a className="font-semibold" href="#">
+                  Home
+                </a>
+                <a className="font-semibold" href="#">
+                  Privacy
+                </a>
+                <a className="font-semibold" href="#">
+                  Terms
+                </a>
+                <a className="font-semibold" href="#">
+                  404
+                </a>
+              </div>
             </div>
           </div>
         </div>
