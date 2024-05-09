b2f56b4b506c11400d17dfaf3743bce4aa8d8a64
Niharika Deb <127780989+nihaa222@users.noreply.github.com>
2024-05-09 23:34:14 +0530

    service animation

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { motion } from "framer-motion";
 
 const Service = ({ id }) => {
   return (
@@ -7,7 +8,7 @@ const Service = ({ id }) => {
       id={id}
       className="section text-center flex-col mt-[130px] flex gap-3"
     >
-      <div className="flex  gap-2 justify-center items-center mb-1">
+      <div className="flex gap-2 justify-center items-center mb-1">
         <img className="h-4 w-4" src="none.png" alt="Service" />
         <p className="text-center text-orange-500">Services</p>
       </div>
@@ -18,9 +19,17 @@ const Service = ({ id }) => {
       </p>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 md:gap-10 max-w-[1400px] mx-auto">
-        <div className="md:h-[400px] rounded-3xl h-[420px] w-[320px] sm:w-[400px] lg:w-[300px]  bg-blue-200 pt-10 mx-auto">
-          <div className="w-[280px] sm:w-[350px] lg:w-[250px] h-[150px] mx-auto  ">
-            <img className="h-full w-full object-fit" src="/phones.jpg"></img>
+        <motion.div
+          initial={{ x: "-100px" }}
+          whileInView={{ x: 0, transition: { delay: 0.2, duration: 0.5 } }}
+          className="md:h-[400px] rounded-3xl h-[420px] w-[320px] sm:w-[400px] lg:w-[300px] bg-blue-200 pt-10 mx-auto"
+        >
+          <div className="w-[280px] sm:w-[350px] lg:w-[250px] h-[150px] mx-auto">
+            <img
+              className="h-full w-full object-fit"
+              src="/phones.jpg"
+              alt="phones"
+            />
           </div>
           <div className="flex flex-col mx-[30px]">
             <p className="text-[32px] mt-8">Build</p>
@@ -29,11 +38,19 @@ const Service = ({ id }) => {
               solutions tailored to your business needs and goals.
             </p>
           </div>
-        </div>
+        </motion.div>
 
-        <div className="md:h-[400px] mt-0 lg:-mt-10 rounded-3xl h-[420px] w-[320px] sm:w-[400px] lg:w-[300px]  bg-purple-100 pt-10 mx-auto">
+        <motion.div
+          initial={{ y: "100px" }}
+          whileInView={{ y: 0, transition: { delay: 0.2, duration: 0.5 } }}
+          className="md:h-[400px] xl:-mt-10 rounded-3xl h-[420px] w-[320px] sm:w-[400px] lg:w-[300px] bg-blue-200 pt-10 mx-auto"
+        >
           <div className="w-[280px] sm:w-[350px] lg:w-[250px] h-[150px] mx-auto bg-white">
-            <img className="h-full w-full object-fit" src="laptop.jpg"></img>
+            <img
+              className="h-full w-full object-fit"
+              src="laptop.jpg"
+              alt="laptop"
+            />
           </div>
           <div className="flex flex-col mx-[30px]">
             <p className="text-[32px] mt-8">Deploy</p>
@@ -42,11 +59,19 @@ const Service = ({ id }) => {
               and optimal performance across all platforms.
             </p>
           </div>
-        </div>
+        </motion.div>
 
-        <div className="md:h-[400px] rounded-3xl h-[420px] w-[320px] sm:w-[400px] lg:w-[300px]   bg-yellow-100 pt-10 mx-auto">
+        <motion.div
+          initial={{ x: "100px" }}
+          whileInView={{ x: 0, transition: { delay: 0.2, duration: 0.5 } }}
+          className="md:h-[400px] rounded-3xl h-[420px] w-[320px] sm:w-[400px] lg:w-[300px] bg-blue-200 pt-10 mx-auto"
+        >
           <div className="w-[280px] sm:w-[350px] lg:w-[250px] h-[150px] mx-auto">
-            <img className="w-full h-full" src="promotion.avif"></img>
+            <img
+              className="w-full h-full"
+              src="promotion.avif"
+              alt="promotion"
+            />
           </div>
           <div className="flex flex-col mx-[30px]">
             <p className="text-[32px] mt-8">Promote</p>
@@ -56,9 +81,10 @@ const Service = ({ id }) => {
               presence.
             </p>
           </div>
-        </div>
+        </motion.div>
       </div>
-      <button className="flex justify-center mt-5 text-white font-bold hover:bg-orange-300 rounded-3xl items-center w-[200px] h-[50px] mx-auto bg-orange-500 ">
+
+      <button className="flex justify-center mt-5 text-white font-bold hover:bg-orange-300 rounded-3xl items-center w-[200px] h-[50px] mx-auto bg-orange-500">
         Book a 15-min call
       </button>
     </section>
